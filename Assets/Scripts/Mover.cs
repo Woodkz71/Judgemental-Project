8b711f9b4c0fa516be422a39194429298ba630a0
Kev <kzwood01@hotmail.com>
2021-06-28 12:16:17 +0100

    Introduction to RayCasting
    Updated mover Script. This allows the camera to have a line show to indicate where the mouse clicked in the Game.

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -7,8 +7,19 @@ public class Mover : MonoBehaviour
 {
     [SerializeField] Transform target;
 
+    Ray lastRay;
+
     private void Update()
     {
+        if (Input.GetMouseButtonDown(0))
+        {
+            lastRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        }
+
+        Debug.DrawRay(lastRay.origin, lastRay.direction * 100);
+
         GetComponent<NavMeshAgent>().destination = target.position;
+
+       
     }
 }
